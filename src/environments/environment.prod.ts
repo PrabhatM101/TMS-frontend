73944b4d83ab2edd3c39dbc228cd0f7e820cdd26
Prabhat Mishra <prabhatkumarmishra101@gmail.com>
2025-08-31 17:11:19 +0530

    TMS frontend init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  socketUrl: 'http://localhost:3000',
+  // apiUrl: 'http://localhost:3000',
+  // socketUrl: 'http://localhost:3000',
+
+
+  apiUrl: 'https://tms-backend-otej.onrender.com',
+  socketUrl: 'https://tms-backend-otej.onrender.com',
 };
